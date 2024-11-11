823ad388141424b57989643add7ff16ca891b41d
BorislavSpasov <borislav.e.spasov@gmail.com>
2024-11-11 18:41:06 +0200

    Add files via upload

---
+++ HomeworkFacebook/ConsoleApp4/Program.cs
@@ -0,0 +1,47 @@
+﻿string personName = Console.ReadLine();
+//checking if the first name is an empty input
+if (personName == "") return;
+
+int extraLikesCounter = 0;
+int numPeopleLiked = 0;
+
+string likeMessage = null;
+string postLikeMessage = "liked your post!";
+string peopleLiked = personName;
+
+numPeopleLiked++;
+
+while (true) { 
+    //checking for how many people liked
+    switch (numPeopleLiked)
+    {
+        case 1:
+            //printing first like message
+            likeMessage = $"{peopleLiked} {postLikeMessage}";
+            Console.WriteLine(likeMessage);
+            break;
+        case 2:
+            peopleLiked += $" and {personName}";
+            likeMessage = $"{peopleLiked} {postLikeMessage}";
+            Console.WriteLine(likeMessage);
+            //replacing the "and" with "," for the default case message
+            peopleLiked = peopleLiked.Replace(" and", ",");
+            break;
+        //when the amount of people that liked is greater than 2
+        default:
+            //counting all extra people that have liked the post
+            extraLikesCounter++;
+            //new end part of message
+            postLikeMessage = $"and {extraLikesCounter} other liked your post!";
+            //printing out message
+            likeMessage = $"{peopleLiked} {postLikeMessage}";
+            Console.WriteLine(likeMessage);
+            break;
+    }
+    //adding to the total num of people that liked
+    numPeopleLiked++;
+    //reading new name
+    personName = Console.ReadLine();
+    //checking if the the new name is an empty input
+    if (personName == "") return;
+}
---
+++ HomeworkTriangle/homework10112024/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.Write("h= ");
+int height = int.Parse(Console.ReadLine());
+Console.Write("pos= ");
+string side = Console.ReadLine();
+
+int sideACords = 0;
+int sideBCords = 0;
+
+Console.Clear();
+
+switch (side)
+{
+    case "left":
+        //printing left side of triangle
+        for (int i = 1; i < height; i++)
+        {
+            Console.WriteLine("*");
+        }
+        //printing right side of triangle
+        for (int i = 0; i < height; i++)
+        {
+            Console.Write("*");
+        }
+        //printing bottom of triangle
+        for (int i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(sideACords, sideBCords);
+            Console.Write("*");
+            sideACords++;
+            sideBCords++;
+        }
+        break;
+
+    case "right":
+
+        sideACords = height;
+        //printing right side of triangle
+        for (int i = 1; i < height; i++)
+        {
+            Console.SetCursorPosition(height - 1, i);
+            Console.WriteLine("*");
+        }
+        //printing left side of triangle
+        for (int i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(sideACords - 1, sideBCords);
+            Console.Write("*");
+            sideACords--;
+            sideBCords++;
+        }
+        //printing bottom of triangle
+        for (int i = 0; i < height; i++)
+        {
+            Console.SetCursorPosition(i, height - 1);
+            Console.Write("*");
+        }
+        break;
+    //checking if the pos is valid
+    default:
+        Console.WriteLine("Invalid position!");
+        break;
+}
